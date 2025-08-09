114c65624d7f57a1ef40794d37b669d5736e27ac
Rul991 <archerartem6@gmail.com>
2025-08-09 23:57:55 +0300

    updated test command

--- src/classes/commands/buckwheat/TestCommand.ts
+++ src/classes/commands/buckwheat/TestCommand.ts
@@ -1,8 +1,10 @@
 import { Context } from 'telegraf'
 import { MaybeString } from '../../../utils/types'
 import BuckwheatCommand from '../base/BuckwheatCommand'
-import ContextUtils from '../../../utils/ContextUtils'
 import MessageUtils from '../../../utils/MessageUtils'
+import CasinoRepository from '../../db/repositories/CasinoRepository'
+import UserRankService from '../../db/services/user/UserRankService'
+import RankUtils from '../../../utils/RankUtils'
 
 export default class TestCommand extends BuckwheatCommand {
     constructor() {
@@ -11,6 +13,17 @@ export default class TestCommand extends BuckwheatCommand {
     }
 
     async execute(ctx: Context, other: MaybeString): Promise<void> {
+        if(await UserRankService.get(ctx.from?.id ?? 0) >= RankUtils.adminRank) {
+            const casinos = await CasinoRepository.findMany()
+            console.log(casinos)
+
+            for (const casino of casinos) {
+                if(casino.money! > 200) {
+                    CasinoRepository.updateOne(casino.id, {money: 200})
+                }
+            }
+        }
+
         await MessageUtils.answerMessageFromResource(ctx, 
             'text/commands/test.html',
             {
