53063f85e851ba3d3b631742fde78941f07074ad
Rul991 <archerartem6@gmail.com>
2025-08-18 09:13:33 +0300

    fixed bug with top command

--- src/utils/ArrayUtils.ts
+++ src/utils/ArrayUtils.ts
@@ -1,7 +1,8 @@
 export default class ArrayUtils {
     static filterAndSort<T extends Record<any, any>>(arr: T[], key: keyof T, maxCount = arr.length) {
         return arr
-            .filter((v, i) => v[key] > 0 && i < maxCount)
+            .filter((v, i) => v[key] > 0)
             .sort((a, b) => (b[key] - a[key]))
+            .filter((v, i) => i < maxCount)
     }
 }
