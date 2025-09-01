39f1fbfbce74484a9789abc909058f3c83089212
Rul991 <archerartem6@gmail.com>
2025-09-01 18:13:08 +0300

    v1.2.3-hf1

--- src/classes/commands/buckwheat/EchoCommand.ts
+++ src/classes/commands/buckwheat/EchoCommand.ts
@@ -4,6 +4,9 @@ import BuckwheatCommand from '../base/BuckwheatCommand'
 import ContextUtils from '../../../utils/ContextUtils'
 import MessageUtils from '../../../utils/MessageUtils'
 import UserLinkedService from '../../db/services/user/UserLinkedService'
+import UserRankService from '../../db/services/user/UserRankService'
+import RankUtils from '../../../utils/RankUtils'
+import Logging from '../../../utils/Logging'
 
 export default class EchoCommand extends BuckwheatCommand {
     constructor() {
@@ -15,7 +18,15 @@ export default class EchoCommand extends BuckwheatCommand {
     }
 
     async execute(ctx: TextContext, other: MaybeString): Promise<void> {
+        const rank = await UserRankService.get(ctx.from.id)
+
+        if(rank < RankUtils.min + 2) {
+            await MessageUtils.answerMessageFromResource(ctx, 'text/commands/echo/rank-issue.pug')
+            return
+        }
+
         if(typeof other == 'string' && other.length) {
+            Logging.log(`${ctx.from.first_name}(${ctx.from.id}) echoed "${other}"`)
             const isPrivate = ctx.chat.type == 'private'
             const chatId = isPrivate ? 
                 await UserLinkedService.get(ctx.from.id) || ctx.chat.id :
@@ -37,7 +48,7 @@ export default class EchoCommand extends BuckwheatCommand {
             await ctx.deleteMessage()
         }
         else {
-            MessageUtils.answerMessageFromResource(ctx, 'text/commands/echo/echoError.pug')
+            await MessageUtils.answerMessageFromResource(ctx, 'text/commands/echo/echoError.pug')
         }
     }
 }
